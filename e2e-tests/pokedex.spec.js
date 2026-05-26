b6dbeba983707ca573a2d6250fe3a12e6a7fad96
jasminlei <jasminleinonen@hotmail.com>
2026-05-26 10:34:35 +0300

    exercise 9: add test to ensure individual pokemon pages can be navigated to, change npm install to npm ci

--- e2e-tests/pokedex.spec.js
+++ e2e-tests/pokedex.spec.js
@@ -2,12 +2,21 @@ import { test, expect } from '@playwright/test'
 
 test.describe('Pokedex', () => {
   test('front page can be opened', async ({ page }) => {
-    await page.goto('http://localhost:8080/')
+    await page.goto('/')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(
       page.getByText(
         'Pokémon and Pokémon character names are trademarks of Nintendo.',
       ),
     ).toBeVisible()
+  })
+
+  test('can navigate to a pokemon page', async ({ page }) => {
+    await page.goto('/')
+
+    await page.getByRole('link', { name: 'ivysaur' }).click()
+
+    await expect(page).toHaveURL(/\/pokemon\/ivysaur$/)
+    await expect(page.getByText('chlorophyll')).toBeVisible()
   })
 })
