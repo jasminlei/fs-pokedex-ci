1e171cbb1390f6e8e812aff32898cf9f190889f0
jasminlei <jasminleinonen@hotmail.com>
2026-05-27 11:02:48 +0300

    exercise 10: deploy the app

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const app = express()
 
 // get the port from env variable
 const PORT = process.env.PORT || 5001
+const HOST = process.env.HOST || '0.0.0.0'
 
 app.use(express.static('dist'))
 
-const start = async () => {
-  await app.listen(PORT)
-  console.log(`server started on port ${PORT}`)
-}
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
 
-start()
+app.listen(PORT, HOST, () => {
+  console.log(`server started on ${HOST}:${PORT}`)
+})
